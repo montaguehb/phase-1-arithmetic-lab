163fe0adbe563d0ac159d2517be7a76edc799c23
Historia Montague <historia.montague@outlook.com>
2023-04-09 20:03:54 -0600

    Finished lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const num3 = 31415;
+const num4 = 2890184;
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num4 % num3;
+let max = Math.max(1, 2, 3, 20);
